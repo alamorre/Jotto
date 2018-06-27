1f82b4958df649dcfbd16986470f5bbb5a196b58
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-27 10:43:28 -0400

    downloaded test dependencies

---
+++ src/Congrats.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+Funational react component for gongrats statement
+@function
+@returns {JSX.Element} - Rendered congrats message to the user
+*/
+export defaul () => {
+  return (
+    <div>
+  );
+}
