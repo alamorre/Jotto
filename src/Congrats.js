d106367b9a3ab4e8798618200bedce16b81c9a38
Adam La Morre <adam.lamorre@mail.mcgill.ca>
2018-06-27 13:04:16 -0400

    added refactored tests for success message

--- src/Congrats.js
+++ src/Congrats.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 /**
 Funational react component for gongrats statement
 @function
+@param {object}props - React props
 @returns {JSX.Element} - Rendered congrats message to the user
 */
-export defaul () => {
-  return (
-    <div>
-  );
+export default (props) => {
+  if(props.success){
+    return(
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congrats, you guessed it!
+        </span>
+      </div>
+    );
+  } else {
+    return(
+      <div data-test="component-congrats" />
+    );
+  }
 }
--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,1 +1,44 @@
 import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+import checkPropTypes from 'check-prop-types';
+import Congrats from './Congrats';
+import {findByTestAttr, checkProps} from '../test/testUtils';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const defaultProps = {success: false};
+
+/**
+* Factory function to create a shallowWrapper for the Congrats component
+* @function setUp
+* @param {object} props - Component props specific to this setUp
+* @returns {ShallowWrapper}
+*/
+const setUp = (props={}) => {
+  const setUpProps = {...defaultProps, ...props}; // if ...props then override
+  return shallow(<Congrats {...setUpProps} />)
+}
+
+test('renders without error', () => {
+  const wrapper = setUp();
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.length).toBe(1);
+});
+
+test('renders with no test with "success" props is false', () => {
+  const wrapper = setUp({success: false});
+  const component = findByTestAttr(wrapper, 'component-congrats');
+  expect(component.text()).toBe("");
+});
+
+test('renders non-empty congrats when success', () => {
+  const wrapper = setUp({success: true});
+  const message = findByTestAttr(wrapper, 'congrats-message');
+  expect(message.text().length).not.toBe(0);
+});
+
+test('does not throw arning with expected props', () => {
+  const expectedProps = {success: false};
+  checkProps(Congrats, expectedProps);
+});
